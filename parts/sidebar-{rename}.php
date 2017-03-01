2759541c63d98bf05a1aafa8b899837b3920e677
David Carter <dajocarter@gmail.com>
2017-03-01 14:11:12 -0500

    Remove some more template declarations

--- parts/sidebar-{rename}.php
+++ parts/sidebar-{rename}.php
@@ -1,4 +1,3 @@
 <aside id="sidebar"
        class="fourcol last">
-  <p>This is the sidebar-{rename}.php file</p>
 </aside>
--- parts/sidebar.php
+++ parts/sidebar.php
@@ -1,4 +1,3 @@
 <aside id="sidebar"
        class="fourcol last">
-  <p>This is the sidebar.php file</p>
 </aside>
